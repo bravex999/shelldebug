6841d955266f16601ce7ee4b8fea5a39c237b23f
naranjox <cdny195@gmail.com>
2025-08-20 15:03:23 +0200

    faltan pipes

--- mini-main/minishell_repo/includes/main_aux.h
+++ mini-main/minishell_repo/includes/main_aux.h
@@ -2,8 +2,8 @@
 # define MAIN_AUX_H
 
 void	start_loop(t_shell *shell);
-int		handle_ctrl_c(char **line);
-int		check_ctrl_d(char *line); // Gestión ctrl D
+int		handle_ctrl_c(char **line, t_shell **line);
+int		check_ctrl_d(char *line);
 char	*read_input(void);
 int		handle_ctrl_c_signal(char **line);
 int		history(char *line);
