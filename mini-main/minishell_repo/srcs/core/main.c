3ccc42d5ac0e169da377cd552fa7a633078fe981
naranjox <cdny195@gmail.com>
2025-08-30 18:13:16 +0200

    j

--- mini-main/minishell_repo/srcs/core/main.c
+++ mini-main/minishell_repo/srcs/core/main.c
@@ -4,8 +4,13 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_shell	shell;
 
-	(void)argc;
-	(void)argv;
+	if (argc > 1)
+	{
+		write(STDERR_FILENO, "minishell: ", 11);
+		write(STDERR_FILENO, argv[1], ft_strlen(argv[1]));
+		write(STDERR_FILENO, ": No such file or directory\n", 28);
+		return (UNKNOWN_COMMAND);
+	}
 	init_shell(&shell, envp);
 	setup_signals();
 	start_loop(&shell);
